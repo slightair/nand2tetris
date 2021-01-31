1432fdcd322cbb42afa415856f45a898b6e4a6de
slightair <arksutite@gmail.com>
2021-01-31 18:49:23 +0900

    chap 3

---
+++ projects/03/a/check.rb
@@ -0,0 +1,10 @@
+problems = Dir.glob(File.join(__dir__, '*.hdl')).sort
+
+problems.each do |x|
+    basename = File.basename(x, ".hdl")
+    cmp = open(File.join(__dir__, "#{basename}.cmp")).read.gsub(/\r\n/, "\n") rescue next
+    out = open(File.join(__dir__, "#{basename}.out")).read rescue nil
+    result = cmp == out ? "✅" : "❌"
+
+    puts "#{basename} - #{result}"
+end
