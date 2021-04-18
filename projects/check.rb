09a21a280885f67d7852aa4b2e2fade1e5e60266
slightair <arksutite@gmail.com>
2021-04-18 11:59:28 +0900

    Improve check script

--- projects/check.rb
+++ projects/check.rb
@@ -1,13 +1,38 @@
-chap = ARGV.first || "00"
+def compare(cmp, out)
+  return true if cmp == out
+  return false unless out
+  return false unless cmp.lines.size == out.lines.size
 
-directory = File.join(__dir__, chap)
-problems = Dir.glob(File.join(directory, '**', '*.cmp')).sort
+  cmp.lines.zip(out.lines) do |cmp_line, out_line|
+    cmp_cells = cmp_line.split('|')
+    out_cells = out_line.split('|')
+    return false unless cmp_cells.size == out_cells.size
+    return false unless cmp_cells.zip(out_cells).all? do |cmp_cell, out_cell|
+      cmp_cell.chars.uniq == ['*'] || cmp_cell == out_cell
+    end
+  end
 
-problems.each do |x|
+  true
+end
+
+chap = ARGV.first
+chapters = chap ? [chap] : (0..13).map { |c| "%02d" % c }
+
+chapters.each do |c|
+  directory = File.join(__dir__, c)
+  problems = Dir.glob(File.join(directory, '**', '*.cmp')).sort
+  next if problems.empty?
+
+  puts "#{c}:"
+
+  problems.each do |x|
     basename = x.sub(directory + File::SEPARATOR, "").chomp(".cmp")
     cmp = open(File.join(directory, "#{basename}.cmp")).read.gsub(/\r\n/, "\n") rescue next
     out = open(File.join(directory, "#{basename}.out")).read rescue nil
-    result = cmp == out ? "✅" : "❌"
+    result = compare(cmp, out) ? "✅" : "❌"
 
-    puts "#{basename} - #{result}"
+    puts "\t#{basename} - #{result}"
+  end
+
+  puts
 end
